c2fb66744be165bc97474e36628905d68bdfd5a4
Sphoorthi-p <>
2019-05-05 20:30:45 -0400

    Create End to end integration test case

---
+++ src/test/java/com/projects/integration/SimpleTest2.java
@@ -0,0 +1,22 @@
+package com.projects.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleTest2 {
+    @Test
+public void openFacebook()
+{
+    System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
+    ChromeDriver driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get("https://www.facebook.com/");
+    Assert.assertEquals("Facebook - Log In or Sign Up",driver.getTitle());
+    driver.findElementByCssSelector("input#email").sendKeys("sphoorthi");
+    driver.findElementByCssSelector("input#pass").sendKeys("sphoorthi");
+    driver.quit();
+}
+}
